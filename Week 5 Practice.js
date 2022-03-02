e81af7197a6b36504646ff8bb7d901cea0b74d44
Mariah Childress <mariahchildress@Mariahs-MacBook-Air.local>
2022-03-01 21:02:08 -0600

    Tuesday Night

--- Week 5 Practice.js
+++ Week 5 Practice.js
@@ -1,1 +1,25 @@
 // Object Oriented Programming //
+
+// Classes //
+class Student {
+    constructor (firstName, lastName, phoneNumber, grade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.grade = grade;
+    }
+
+    introduce() {
+        console.log(`${this.firstName} ${this.lastName} can be reached at ${this.phoneNumber}`)
+    }
+}
+
+let student = new Student();
+student.firstName = 'Tommy';
+student.lastName = 'Smith';
+
+let student1 = new Student('Tom', 'Sawyer', '6295555555', 'A');
+let student2 = new Student('Sam', 'Smith', '4895555555', 'A');
+
+student1.introduce();
+student2.introduce();
